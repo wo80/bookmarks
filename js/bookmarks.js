5552150b6cb7702468ae5e892a73c862811cb396
wo80 <christian.woltering@tu-dortmund.de>
2015-03-03 15:47:19 +0100

    commit new version (v0.2)

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,221 +1,72 @@
-// JSON Bookmark Viewer
+;(function(exports) {
 
-var app = (function () {
 	"use strict";
-
-	var drop, db, folders, bookmarks,
+	
+	var drop, db, folders, listview, jsp,
 		current = null; // Element corresponding to the current folder
 
-	function DropTarget(selector, callback) {
-		/// <summary>
-		/// Helper class for handling dropping of files onto the
-		/// browser window.
-		/// </summary>
-		var el = document.querySelector(selector);
-
-		this.dummy = function(e) {
-			e.stopPropagation();
-			e.preventDefault();
+	if (!Array.prototype.clone) {
+		Array.prototype.clone = function() {
+			return this.slice(0);
 		};
-
-		this.drop = function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-
-			callback(e.dataTransfer.files, e);
-		};
-
-		el.addEventListener('dragenter', this.dummy, false);
-		el.addEventListener('dragover', this.dummy, false);
-		el.addEventListener('dragleave', this.dummy, false);
-		el.addEventListener('drop', this.drop, false);
 	}
-
-	var getPathUp = function(el) {
-		/// <summary>
-		/// Gets the path from a given node up to the root of the tree.
-		/// </summary>
-		/// <param type="DOM element" name="el">Clicked tree node.</param>
-		/// <returns type="Array">Array of tree node ids containing the path form
-		/// given node up to the root.</returns>
-		var p, path = [el.data('id')];
-
-		while (true) {
-			path.push(el.data('parent'));
-
-			p = el.parent();
-			if (!p.hasClass('container')) {
-				// Top of the tree
-				break;
-			}
-
-			el = p.prev(); // should always exist
-		}
-
-		return path;
-	};
-
-	var findFolder = function(path) {
-		/// <summary>
-		/// Gets the children of the end node of given path.
-		/// </summary>
-		/// <param type="Array" name="path">A path in the tree [node, ... , root].</param>
-		/// <returns type="Array">Contents of end node of given path.</returns>
-		var folder = db.children, i, n,
-			k = path.length - 2; // start one below root
-
-		while (k >= 0) {
-			n = folder.length;
-
-			for (i = 0; i < n; i += 1) {
-				if (folder[i].id == path[k]) {
-					folder = folder[i].children;
-					break;
-				}
-			}
-
-			k -= 1;
-		}
-
-		return folder;
-	};
-
-	var findBookmarks = function(text, root) {
-		/// <summary>
-		/// Search all bookmark nodes of the subtree for given text.
-		/// </summary>
-		/// <param type="String" name="text">String to search for.</param>
-		/// <param type="Object" name="root">Node to begin the search at.</param>
-		/// <returns type="Array">All bookmark nodes which title or uri contain the
-		/// given text.</returns>
-		var items = [], c, b, i, n,
-			re = new RegExp(text, 'i'),
-			stack = [root]; // push root onto stack
-
-		// Loop as long there are children on the stack (this is a
-		// depth-first search of the subtree)
-		while (stack.length > 0) {
-			c = stack.pop();
-
-			// Current node has children
-			if (c.children) {
-				n = c.children.length;
-
-				for (i = 0; i < n; i += 1) {
-					b = c.children[i];
-
-					if (b.uri) {
-						if (re.test(b.uri)) {
-							items.push(b);
-						} else if (b.title && re.test(b.title)) {
-							items.push(b);
-						}
-					} else if (b.children) {
-						// Push child node onto stack
-						stack.push(b);
-					}
-				}
-			}
-		}
-
-		return items;
-	};
-
-	var search = function(text) {
-		/// <summary>
-		/// Find all bookmarks with given text.
-		/// </summary>
-		/// <param type="String" name="text">String to search for.</param>
-		var items = findBookmarks(text, db);
-
-		// Show the bookmarks
-		displayBookmarks(items);
-	};
 
 	var handleClick = function(el) {
 		/// <summary>
 		/// Handle click on a bookmarks folder to open or close the subtree.
 		/// </summary>
 		/// <param type="DOM element" name="el">Clicked tree node.</param>
-		var folder, items, path = getPathUp(el);
+		var folder, items, i, n, ul, path = el.data("path");
 
-		folder = findFolder(path);
+		folder = db.getFolder(path);
 
 		if (el.next().hasClass('container')) {
 			// A subtree exists, so close folder
-			items = getBookmarks(folder);
-			displayBookmarks(items);
+			el.next().remove();
+			el.removeClass('active');
 
 			// TODO: instead of removing elements from the DOM on folder close
 			// and re-add on opening a second time, one could just hide the
 			// subtree and then check if it already exists and show again...
-
-			el.next().remove();
-			el.removeClass('active');
 		} else {
 			// No subtree, so append folder
-			items = appendFolder(el, folder, path.length - 1);
-			displayBookmarks(items);
+			appendFolder(el, folder.folders, path.length);
 		}
+
+		ul = $('<ul>');
+		createBookmarks(ul, folder.bookmarks);
+		listview.empty().append(ul);
 
 		if (current) {
 			// Remove current selection
 			current.removeClass('selected');
 		}
 
+		// Set path information
+		updatePath($("#path"), folder.path);
+
 		// Update selection
 		el.addClass('selected');
 		current = el;
 	};
 
-	var countSubFolders = function(folder) {
-		/// <summary>
-		/// Count the subfolders of given folder.
-		/// </summary>
-		/// <param type="Object" name="folder">A bookmarks folder.</param>
-		var i, n, count = 0, uris = 0, children = folder.children;
+	var updatePath = function(el, path) {
+		var ul, i, n = path.length;
 
-		if (!children) {
-			return 0;
-		}
-
-		n = children.length;
-
-		for (i = 0; i < n; i += 1) {
-			if (children[i].children) {
-				count++;
-			} else if (children[i].uri) {
-				uris++;
+		if (n > 0) {
+			el.empty();
+			for (i = 0; i < n; i += 1) {
+				el.append('<li class="item">' + path[i] + '</li>');
 			}
 		}
-		return count;
 	};
 
-	var getBookmarks = function(children) {
-		/// <summary>
-		/// Get bookmarks in given collection.
-		/// </summary>
-		/// <param type="Array" name="children">The children contained in
-		/// current folder.</param>
-		var i, n = children.length, items = [], b;
-
-		for (i = 0; i < n; i += 1) {
-			b = children[i];
-
-			if (b.uri) {
-				items.push(b);
-			}
-		}
-
-		return items;
-	};
-
-	var displayBookmarks = function(items) {
+	var createBookmarks = function(container, items) {
 		/// <summary>
 		/// Display bookmarks of current folder.
 		/// </summary>
 		/// <param type="Array" name="items">Bookmarks of current folder.</param>
-		var i, n = items.length, b, ul = $('<ul>'), li, link;
+		var i, n = items.length, b, li, link;
 		
 		for (i = 0; i < n; i += 1) {
 			b = items[i];
@@ -226,14 +77,12 @@ var app = (function () {
 			li = $('<li class="bookmark"></li>');
 			li.append($('<div class="title">' + b.title + '</div>'));
 			li.append(link);
-			ul.append(li);
-		}
 
-		bookmarks.find('ul').remove();
-		bookmarks.append(ul);
+			container.append(li);
+		}
 	};
 
-	var appendFolder = function(parent, children, level) {
+	var appendFolder = function(parent, folders, level) {
 		/// <summary>
 		/// Display the next level of folders.
 		/// </summary>
@@ -242,78 +91,67 @@ var app = (function () {
 		/// <param type="Integer" name="level">Level of current folder (depth of path
 		/// in the tree).</param>
 		/// <returns type="Array">Bookmarks in current folder.</returns>
-		var el, b, i, n = children.length, sub,
-			container, items = [];
+		var el, i, count, n = folders.length, container, item;
 		
 		// Create a new container node for folder.
 		container = $('<div class="container L'+ level +'">');
 
 		for (i = 0; i < n; i += 1) {
-			b = children[i];
+			item = folders[i];
 
-			if (b.uri) {
-				// Child is a bookmark
-				items.push(b);
-			} else if (b.title != "") {
-				// Child is a folder
-				sub = countSubFolders(b);
+			// Create new folder element
+			el = $('<div class="folder">' + item.title + '</div>');
+			el.data('path', item.path);
 
-				// Create new folder element
-				el = $('<div class="folder">' + b.title + '</div>');
-				el.data('id', b.id);
-				el.data('parent', b.parent);
-				el.data('count', sub);
+			count = item.count;
 
-				if (sub > 0) {
-					el.append('<span class="count">' + sub + '</span>');
-				} else {
-					el.addClass('empty');
-				}
-
-				container.append(el);
+			if (count.bookmarks > 0) {
+				el.append('<span class="count">' + count.bookmarks + '</span>');
 			}
+			
+			if (count.folders === 0) {
+				el.addClass('empty');
+			}
+
+			container.append(el);
 		}
 
 		// Set parent to active and append subfolders.
-		if (parent.data('count') != 0) {
-			parent.addClass('active');
-			parent.after(container);
-		}
-
-		return items;
+		parent.addClass('active');
+		parent.after(container);
 	};
 
-	var loadRoot = function() {
+	var loadRoot = function(json) {
 		/// <summary>
 		/// Display all folders of the tree root.
 		/// </summary>
-		var children, i, n, sub, el, b;
+		var i, n, el, folder, items;
 
 		// Check Firefox bookmarks format
-		if (!(db.id == 1 && db.root)) {
-			showError("Error: somethings wrong with the JSON file.");
+		if (MozBookmarks.validate(json)) {
+			db = new MozBookmarks(json);
+		} else if (WebKitBookmarks.validate(json)) {
+			db = new WebKitBookmarks(json);
+		} else {
+			showError("Unknown JSON format");
 			return;
 		}
 
-		children = db.children;
-		n = children.length;
+		items = db.getRoot();
+		n = items.length;
 
 		for (i = 0; i < n; i += 1) {
-			b = children[i];
-			sub = countSubFolders(b);
+			folder = items[i];
 
-			el = $('<div class="folder">' + b.title + '</div>');
-			el.data('id', b.id);
-			el.data('parent', b.parent);
-			el.data('count', sub);
+			el = $('<div class="folder">' + folder.title + '</div>');
+			el.data('path', folder.path);
 
-			if (sub > 0) {
-				el.append('<span class="count">' + sub + '</span>');
+			if (folder.count > 0) {
+				el.append('<span class="count">' + folder.count + '</span>');
 			}
 
 			folders.append(el);
 		}
-
 	};
 
 	var loadBookmarksCallback = function(json) {
@@ -321,13 +159,14 @@ var app = (function () {
 		/// Callback for loading functions (local file reader or ajax).
 		/// </summary>
 		/// <param type="JSON object" name="json">Bookmarks in JSON format.</param>
-		db = json;
 
 		// Clear lists
 		folders.empty();
-		bookmarks.find('ul').remove();
+		listview.find('ul').remove();
 
-		loadRoot();
+		loadRoot(json);
+
+		jsp.reinitialise();
 	};
 	
 	var loadBookmarksAjax = function(url) {
@@ -370,31 +209,6 @@ var app = (function () {
 		}
 	};
 
-
-	var toggleAbout = function() {
-		var left, top, about = $('#about');
-
-		if (!about.is(':visible')) {
-			left = ($(document).width() - about.width()) / 2;
-			top = ($(document).height() - 40 - about.height()) / 2;
-
-			about.css('left', left + 'px');
-			about.css('top', top + 'px');
-		}
-
-		$('#about').toggle();
-	};
-
-	var toggleSearch = function() {
-		var search = $('#search');
-
-		search.toggle();
-		
-		if (search.is(':visible')) {
-			search.find('input').focus();
-		}
-	};
-
 	var showError = function(message) {
 		if (message) {
 			var el = $('#error');
@@ -403,52 +217,55 @@ var app = (function () {
 		}
 	};
 
-	var resize = function() {
-		var h = $(document).height() - $('#header').height() - 20;
+	var search = function(text) {
+		var i, n, folder, ul, result;
 
-		folders.height(h);
-		bookmarks.height(h);
-    };
+		if (text.length < 1) {
+			return;
+		}
+
+		listview.empty();
+		result = db.search(text);
+
+		if (result.count === 0) {
+			return;
+		}
+
+		updatePath($("#path"), ["Search Results"]);
+
+		ul = $('<ul>');
+		n = result.folders.length;
+		for (i = 0; i < n; i += 1) {
+			folder = result.folders[i];
+			createBookmarks(ul, folder.items, folder.path); // TODO: visualize folder path
+		}
+		listview.append(ul);
+		
+		//listview.unhighlight()
+		//listview.highlight(text);
+	};
 
 	var init = function(url) {
-
 		folders = $('#folders');
-		bookmarks = $('#bookmarks');
+		listview = $('.listview');
 
-		resize();
+		jsp = $('.scroll-container').jScrollPane({
+			maintainPosition: true
+		}).data('jsp');
 
 		// Delegate click event on folders
 		folders.on('click', '.folder', function(ev) {
 			handleClick($(ev.currentTarget));
+			jsp.reinitialise();
 		});
 
-		// Toggle search box
-		$('a.search').click(function(e) {
-			toggleSearch();
-			e.preventDefault();
-		});
-
-		// Toggle about box
-		$('a.about').click(function(e) {
-			toggleAbout();
-			e.preventDefault();
-		});
-
-		// Close about box
-		$('#about .close').click(function(e) {
-			$('#about').hide();
-			e.preventDefault();
-		});
-		
 		// Search box key bindings
-		$('#search input').keyup(function(e) {
+		$('input.search').keyup(function(e) {
 			if (e.keyCode === 13) { // Enter
-				var text = $('#search input').val();
+				var text = $(this).val();
 				if (text !== '') {
 					search(text);
 				}
-			} else if (e.keyCode === 27) { // Esc
-				toggleSearch();
 			}
 		});
 
@@ -460,7 +277,7 @@ var app = (function () {
 		// Enable dropping JSON mesh files
 		if (window.File && window.FileReader) {
 			// File API supported
-			drop = new DropTarget('body', function(files) {
+			new DropTarget('body', function(files) {
 				loadBookmarksLocal(files);
 			});
 		}
@@ -470,7 +287,9 @@ var app = (function () {
 		}
 	};
 
-	return {
+	var app = {
 		init: init
 	};
-})();
+	exports.app = app;
+
+}(typeof exports === 'object' && exports || this));
